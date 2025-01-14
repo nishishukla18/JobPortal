7fd926b8aec2a34a537c02828c967a0f64ef1ff9
Nishi Shukla <nishishukla772@gmail.com>
2025-01-14 19:42:40 +0530

    new changes

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', () => console.log('Database Connected'))
-        await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
